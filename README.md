2d8400aa57ae207b6f635ffc793502803e1472b7
powcoder <powcoder@163.com>
2022-11-25 15:35:05 +0800

    COMP6714 Information Retrieval and Web Search

---
+++ README.md
@@ -0,0 +1,7 @@
+# COMP6714 Information Retrieval and Web Search
+# 加微信 powcoder
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
---
+++ project2/project_part1.py
@@ -0,0 +1,10 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+def WAND_Algo(query_terms, top_k, inverted_index):
+    pass ## Replace this line with your implementation...!
